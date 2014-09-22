d558265c1ed44f64d0eba0d0671970b2bfef7140
Caspar Friedrich <caspar.friedrich@koeln.de>
2014-09-22 15:38:27 +0200

    Neues Installationsscript für bonescript

---
+++ node/install-bonescript.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+git clone -C /tmp https://github.com/jadonk/bonescript.git
+npm install /tmp/bonescript/
--- scripts/install-boneserver.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-git clone https://github.com/jadonk/bonescript.git
-npm install --python="/bin/python2" ./bonescript/
-rm -r bonescript/
