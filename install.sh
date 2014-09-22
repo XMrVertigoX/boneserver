6057174f6418df9ec5f737456607738f8aea7340
Caspar Friedrich <caspar.friedrich@koeln.de>
2014-09-22 17:48:49 +0200

    haproxy und andere

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+# basic software
+pacman -S base-devel python2 nodejs ntp git lighttpd linux-headers-am33x-legacy
+
+ln -s /bin/python2 /bin/python
+
+# haproxy
---
+++ packages/README.md
@@ -0,0 +1,4 @@
+boneserver - packages
+=====================
+
+Hier liegen pakete, die nicht in den Repositories oder nicht für die ARM-Architektur verfügbar sind.
