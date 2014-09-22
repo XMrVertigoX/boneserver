e02dfc7448a0ea93d6f8b351c400d761ad3297ff
Caspar Friedrich <caspar.friedrich@koeln.de>
2014-09-22 18:03:45 +0200

    vsftpd.conf

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #! /bin/sh
 
 # basic software
-pacman -S base-devel python2 nodejs ntp git lighttpd linux-headers-am33x-legacy
+pacman -S --noconfirm base-devel python2 nodejs ntp git lighttpd linux-headers-am33x-legacy vsftpd
 
+# haproxy from local package
+pacman -U /opt/boneserver/packages/haproxy-1.5.3-1-armv7h.pkg.tar.xz
+
+# Link configuration files
 ln -s /bin/python2 /bin/python
 
-# haproxy
+mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.old
+ln -sf /opt/boneserver/config/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf
+
+mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.old
+ln -sf /opt/boneserver/config/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg
+
+mv /etc/vsftpd.conf /etc/vsftpd.conf.old
+ln -sf /opt/boneserver/config/vsftpd/vsftpd.conf /etc/vsftpd.conf
