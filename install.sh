e36fd526473072ca04ad36b3a79d8fb48ec66334
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2014-09-24 04:54:56 +0200

    nightly

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-#! /bin?sh
+#! /bin/sh
+
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 
 systemctl stop bonescript.service
 systemctl stop bonescript.socket
@@ -15,15 +17,15 @@ systemctl disable cloud9.socket
 service apache2 stop
 service apache2 remove
 
-apt-get remove --purge -y apache2 lightdm
-apt-get autoremove
+#apt-get remove --purge -y apache2 lightdm
+#apt-get autoremove
 
 tar -xzf packages/haproxy-1.5.3.tar.gz -C /tmp
 cd /tmp/haproxy-1.5.3
-make TARGET=linux2628 USE_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1
+make TARGET=linux2628 USE_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1 clean all
 make install
 cd /opt/boneserver
 
 tar -xzf package/lighttpd-1.4.35.tar.gz -C /tmp
 
-mkdir /var/log/lighttpd
+npm install -g ws
